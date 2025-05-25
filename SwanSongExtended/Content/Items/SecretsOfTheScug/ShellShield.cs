14f5e6bfae4b042d6e5c4a7184cd7ea70ffc27e5
BORBO <bwofodol@gmail.com>
2025-05-25 17:12:50 -0500

    fix shell shield

--- SwanSongExtended/Content/Items/SecretsOfTheScug/ShellShield.cs
+++ SwanSongExtended/Content/Items/SecretsOfTheScug/ShellShield.cs
@@ -87,16 +87,15 @@ namespace SwanSongExtended.Items
         }
         private void ShellShieldOnTakeDamage(On.RoR2.HealthComponent.orig_TakeDamageProcess orig, RoR2.HealthComponent self, RoR2.DamageInfo damageInfo)
         {
-
             CharacterBody body = self?.body;
             int itemCount = GetCount(body);
-            if (itemCount <= 0 || body.notMovingStopwatch <= 0.1f || shellShieldCooldown)
+            if (itemCount <= 0 || body.notMovingStopwatch <= 0.1f || body.HasBuff(shellShieldCooldown))
             {
                 orig(self, damageInfo);
                 return;
             }
             damageInfo.damage = 0;//janky hack mate
-            //damageInfo.rejected = true;
+            damageInfo.rejected = true;
             orig(self, damageInfo);
             ShellShieldBarrier(self, itemCount);
         }
