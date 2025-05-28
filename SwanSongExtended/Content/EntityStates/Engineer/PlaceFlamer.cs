2d54f90f596c77ee07027a6fe50ea8ba3254f420
BORBO <bwofodol@gmail.com>
2025-05-28 15:13:27 -0500

    added legacy content to swansong

---
+++ SwanSongExtended/Content/EntityStates/Engineer/PlaceFlamer.cs
@@ -0,0 +1,19 @@
+﻿using EntityStates.Engi.EngiWeapon;
+using RiskierRainContent.Skills;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RiskierRainContent.States
+{
+    class PlaceFlamer : PlaceTurret
+    {
+        public override void OnEnter()
+        {
+            //base.wristDisplayPrefab = Utils.Paths.GameObject.EngiTurretWristDisplay.Load<GameObject>();
+            //base.blueprintPrefab = Utils.Paths.GameObject.EngiTurretBlueprints.Load<GameObject>();
+            base.turretMasterPrefab = PlaceFlamerTurret.FlamerTurretMaster;
+            base.OnEnter();
+        }
+    }
+}
