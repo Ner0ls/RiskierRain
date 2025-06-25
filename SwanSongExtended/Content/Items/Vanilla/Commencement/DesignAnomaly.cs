859e989d6d383b604dbab925a5f06c0805ec0675
BORBO <bwofodol@gmail.com>
2025-06-25 06:19:44 -0500

    fix design desc and selfdmg safeguards

--- SwanSongExtended/Content/Items/Vanilla/Commencement/DesignAnomaly.cs
+++ SwanSongExtended/Content/Items/Vanilla/Commencement/DesignAnomaly.cs
@@ -6,7 +6,9 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
+using UnityEngine.Networking;
 using static R2API.RecalculateStatsAPI;
+using static SwanSongExtended.Modules.Language.Styling;
 
 namespace SwanSongExtended.Items
 {
@@ -20,18 +22,25 @@ namespace SwanSongExtended.Items
         [AutoConfig("Backstab Damage Reduction", 0.4f)]
         public static float backstabDamageReduction = 0.4f;
         #endregion
+        public static BuffDef beetleArmor;
+        public static int maxBeetleArmorStacks = 3;
+        public static int durationPerBeetleArmor = 3;
+        public static int armorPerBuffBase = 50;
+        public static int armorPerBuffStack = 25;
+        public static float retaliateCrippleDuration = 9f;
         public override AssetBundle assetBundle => SwanSongPlugin.orangeAssetBundle;
         public override string ItemName => "Relic of Design";
 
         public override string ItemLangTokenName => "DESIGNANOMALY";
 
-        public override string ItemPickupDesc => "Reduce damage taken from behind.";
+        public override string ItemPickupDesc => "Periodically gain protection from damage.";
 
-        public override string ItemFullDescription => $"Reduce all damage taken from enemies behind you " +
-            $"by <style=cIsHealth>-{Tools.ConvertDecimal(backstabDamageReduction)}</style> " +
-            $"<style=cStack>(-{Tools.ConvertDecimal(backstabDamageReduction)} per stack)</style>. " +
-            $"Gain <style=cIsHealing>{bonusArmor} passive armor</style> " +
-            $"<style=cStack>(+{bonusArmor} per stack)</style>.";
+        public override string ItemFullDescription => $"After not taking damage for {UtilityColor($"{maxBeetleArmorStacks}")} seconds, " +
+            $"gain a stack of {DamageColor("Chimera Armor")}, up to {UtilityColor($"{maxBeetleArmorStacks}")} times. " +
+            $"Each stack of {DamageColor("Chimera Armor")} " +
+            $"increases {HealingColor("armor")} by {HealingColor($"{armorPerBuffBase}")} {StackText("+" + armorPerBuffStack)}. " +
+            $"Taking damage while protected strips 1 stack of {DamageColor("Chimera Armor")}," +
+            $"{DamageColor("Crippling")} the enemy who attacked you for {retaliateCrippleDuration}s.";
 
         public override string ItemLore => "";
 
@@ -48,16 +57,36 @@ namespace SwanSongExtended.Items
             return null;
         }
 
+        public override void Init()
+        {
+            base.Init();
+        }
+
         public override void Hooks()
         {
-            On.RoR2.HealthComponent.TakeDamageProcess += BackstabDamageReduction;
+            //On.RoR2.HealthComponent.TakeDamageProcess += BackstabDamageReduction;
+            On.RoR2.CharacterBody.OnInventoryChanged += AddItemBehavior;
             GetStatCoefficients += ArmorBoost;
+        }
+
+        private void AddItemBehavior(On.RoR2.CharacterBody.orig_OnInventoryChanged orig, RoR2.CharacterBody self)
+        {
+            orig(self);
+            if (NetworkServer.active)
+            {
+                self.AddItemBehavior<DesignAnomalyBehavior>(GetCount(self));
+            }
         }
 
         private void ArmorBoost(CharacterBody sender, StatHookEventArgs args)
         {
             int itemCount = GetCount(sender);
-            args.armorAdd += itemCount * bonusArmor;
+            int buffCount = sender.GetBuffCount(beetleArmor);
+            if(itemCount > 0 && buffCount > 0)
+            {
+                int armorPerBuff = armorPerBuffBase + armorPerBuffStack * (itemCount - 1);
+                args.armorAdd += armorPerBuff * buffCount;
+            }
         }
 
         private void BackstabDamageReduction(On.RoR2.HealthComponent.orig_TakeDamageProcess orig, RoR2.HealthComponent self, RoR2.DamageInfo damageInfo)
@@ -83,6 +112,67 @@ namespace SwanSongExtended.Items
                 }
             }
             orig(self, damageInfo);
+        }
+    }
+
+    public class DesignAnomalyBehavior : CharacterBody.ItemBehavior
+    {
+        float beetleArmorInterval = DesignAnomaly.durationPerBeetleArmor;
+        float beetleArmorStopwatch;
+        void OnBeetleArmorGained()
+        {
+            GlobalEventManager.onServerDamageDealt += OnServerDamageDealt;
+        }
+        void OnBeetleArmorRemoved()
+        {
+            GlobalEventManager.onServerDamageDealt -= OnServerDamageDealt;
+        }
+
+        private void OnServerDamageDealt(DamageReport damageReport)
+        {
+            if (damageReport.victimBody != this.body || damageReport.attackerBody == this.body)
+                return;
+            if (damageReport.damageInfo.procCoefficient == 0)
+                return;
+            if (damageReport.damageInfo.damageType.damageType.HasFlag(DamageType.Silent))
+                return;
+
+            int buffCount = body.GetBuffCount(DesignAnomaly.beetleArmor);
+            if (buffCount > 0)
+            {
+                body.RemoveBuff(DesignAnomaly.beetleArmor);
+                if (damageReport.attackerBody)
+                    damageReport.attackerBody.AddTimedBuff(RoR2Content.Buffs.Cripple, DesignAnomaly.retaliateCrippleDuration);
+            }
+            if(buffCount <= 1)
+            {
+                OnBeetleArmorRemoved();
+            }
+        }
+
+        private void FixedUpdate()
+        {
+            if (!NetworkServer.active)
+                return;
+            int buffCount = body.GetBuffCount(DesignAnomaly.beetleArmor);
+            if(buffCount >= DesignAnomaly.maxBeetleArmorStacks)
+            {
+                beetleArmorStopwatch = beetleArmorInterval;
+                return;
+            }
+            beetleArmorStopwatch -= Time.fixedDeltaTime;
+            if(beetleArmorStopwatch <= 0)
+            {
+                beetleArmorStopwatch += beetleArmorInterval;
+                body.AddBuff(DesignAnomaly.beetleArmor);
+                if (buffCount == 0)
+                    OnBeetleArmorGained();
+            }
+        }
+
+        private void OnDisable()
+        {
+            this.body.RemoveBuff(CobaltShield.cobaltDefense);
         }
     }
 }
