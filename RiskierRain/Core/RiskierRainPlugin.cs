2f0a9f073887156c2c56e4df848534833399d0b2
BORBO <bwofodol@gmail.com>
2025-07-05 15:57:50 -0500

    version

--- RiskierRain/Core/RiskierRainPlugin.cs
+++ RiskierRain/Core/RiskierRainPlugin.cs
@@ -61,7 +61,7 @@ namespace RiskierRain
         public const string guid = "com." + teamName + "." + modName;
         public const string teamName = "RiskOfBrainrot";
         public const string modName = "RiskierRain";
-        public const string version = "1.1.4";
+        public const string version = "1.1.6";
 
         public static PluginInfo PInfo { get; private set; }
 
