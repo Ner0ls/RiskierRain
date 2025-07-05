44c65cccf2c1657e039de1c00cfb5ee8934a0c87
BORBO <bwofodol@gmail.com>
2025-07-05 15:40:18 -0500

    star veil ignores proc coef check for self dmg

--- SwanSongExtended/Content/Items/SwanSong/StarVeil.cs
+++ SwanSongExtended/Content/Items/SwanSong/StarVeil.cs
@@ -87,8 +87,9 @@ THE SOULS OF MY ????? WILL DRINK YOUR SCREAMS LIKE NECTAR.";
                 {
                     float iframes = GetStackValue(iframeDurationBase, iframeDurationStack, itemCount);
                     self.body.AddTimedBuffAuthority(RoR2Content.Buffs.Immune.buffIndex, iframes);
+                    bool selfDamage = damageInfo.attacker == self.gameObject;
 
-                    if(damageInfo.procCoefficient > 0)
+                    if (damageInfo.procCoefficient > 0 || selfDamage)
                     {
                         MeteorStormController stormController =
                             UnityEngine.Object.Instantiate<GameObject>(Resources.Load<GameObject>("Prefabs/NetworkedObjects/MeteorStorm"),
