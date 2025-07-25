170a656afc2e2463bb02fe325fece0305650e1eb
BORBO <bwofodol@gmail.com>
2025-07-25 02:08:05 -0500

    fix corpsebloom desc?

--- SwanSongExtended/Content/Items/Reworks/LunarHealthDegen.cs
+++ SwanSongExtended/Content/Items/Reworks/LunarHealthDegen.cs
@@ -46,7 +46,7 @@ namespace SwanSongExtended.Items
 
         public override string ItemPickupDesc => "Your health degenerates over time. Gain barrier and luck at low health.";
 
-        public override string ItemFullDescription => $"For every missing {ConvertDecimal(1 / LunarHealthDegenBehavior.maxBuffCount)} of health, gain a stack of Rot. " +
+        public override string ItemFullDescription => $"For every missing {ConvertDecimal(1f / (float)LunarHealthDegenBehavior.maxBuffCount)} of health, gain a stack of Rot. " +
             $"After {rotStacksForFirstBonus} stacks have accumulated, increase base damage by {damageBase} {StackText($"+{damageStack}")}. " +
             $"After {rotStacksForFirstBonus} stacks, gain {ConvertDecimal(LunarHealthDegenBehavior.barrierFraction)} barrier " +
             $"and increase Luck by {luckBase} {StackText($"+{luckStack}")}. " +
